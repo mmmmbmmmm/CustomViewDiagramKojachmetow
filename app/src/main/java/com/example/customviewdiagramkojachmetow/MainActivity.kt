22089d25df2c0d4027bc4c0f1a32160b898cdf12
mmmmbmmmm <95270216+mmmmbmmmm@users.noreply.github.com>
2023-03-01 17:46:50 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/customviewdiagramkojachmetow/MainActivity.kt
+++ app/src/main/java/com/example/customviewdiagramkojachmetow/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.customviewdiagramkojachmetow
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { //Класс для запуска приложения(функция onCreate(), в которой фактически и создается весь интерфейс приложения) 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
